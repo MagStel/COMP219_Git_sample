d54e54816b6baa0ca8e96a6947702dd619e79c9a
Shitoshna <shitoshna_rastogi.sias21@krea.ac.in>
2023-09-21 16:56:16 +0530

    Modify test_code.py to compute average salaries

--- test_code.py
+++ test_code.py
@@ -1,9 +1,26 @@
-# test_code.py
+# Import the pandas library
+import pandas as pd
 
-def read_file(file_name):
-    with open(file_name, 'r') as file:
-        data = file.read()
-    return data
+# Load the CSV data into a pandas DataFrame
+data = pd.read_csv("job_data.csv")
 
-file_name = "sample_data1.txt"
-print(read_file(file_name))
+# Define the job titles for which you want to compute the average salary
+job_titles = ["Data Architect", "Senior Business Analyst", "Data Scientist", "Machine Learning Engineer"]
+
+# Initialize a dictionary to store the average salaries for each job title
+average_salaries = {}
+
+# Compute the average salary for each job title
+for title in job_titles:
+    # Filter the data for the current job title
+    filtered_data = data[data["job_title"] == title]
+    
+    # Calculate the average salary for the filtered data
+    average_salary = filtered_data["num_of_salaries"].mean()
+    
+    # Store the result in the dictionary
+    average_salaries[title] = average_salary
+
+# Print the average salaries
+for title, average_salary in average_salaries.items():
+    print(f"Average Salary for {title}: {average_salary:.2f}")
